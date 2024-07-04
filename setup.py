1ccd36e30a85f5d0a351403671f034f7eaf19d5c
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-07-04 02:31:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
 import subprocess
 import sys
+from colorama import Fore, Style, init, deinit, Back
+import random
+import string
+import os
+import re
+import questionary
+import time
+from alive_progress import alive_bar
+from banner.banner import fun_prompt, twinkling_text, print_mixed_colors_spider
+import sys
+from termcolor import colored
+
+init(autoreset=True)
+
+fun_prompt()
 
 def install_packages():
     packages = [
@@ -18,8 +33,9 @@ def install_packages():
             sys.exit(1)
 
 def main():
-    print("Bienvenue dans l'installation de Promptist Demo par TRHACKNON")
-    print("Ce script va installer les dépendances nécessaires pour exécuter l'application.")
+    # print("Bienvenue dans l'installation de Promptist Demo par TRHACKNON")
+    twinkling_text("Installation de Promptist Demo par TRHACKNON", twinkling_duration=4)
+    print(colored("Ce script va installer les dépendances nécessaires pour exécuter l'application.",'cyan'))
 
     while True:
         user_input = input("Souhaitez-vous continuer avec l'installation ? (oui/non): ").strip().lower()
